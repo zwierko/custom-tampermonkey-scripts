46fcd04074f33d4ad2bc144e332f1051191a6adb
Adam <59660442+zwierko@users.noreply.github.com>
2025-06-01 20:30:23 -0400

    Adding Revolver News cleaner

---
+++ userscripts/revolver_cleaner.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Revolver Cleaner
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-01
+// @description  Clean up articles I don't want to see (orig. 31-May-2025)
+// @author       ajz
+// @require      https://code.jquery.com/jquery-3.7.1.slim.min.js
+// @match        https://revolver.news
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    var $ = jQuery;
+
+    $(document).ready(function() {
+        $('h3:contains("Sponsored")').parents('article').remove();
+        $('a:contains("OneTaste")').parents('article').remove();
+    });
+
+})();
