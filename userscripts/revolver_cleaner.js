ae6dfc4371fc744689000cfe4659ada7b2f4af48
Adam <59660442+zwierko@users.noreply.github.com>
2025-11-10 15:00:28 -0500

    Added Axios removal.

--- userscripts/revolver_cleaner.js
+++ userscripts/revolver_cleaner.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Revolver Cleaner
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-01
+// @version      2025-11-10
 // @description  Clean up articles I don't want to see (orig. 31-May-2025)
 // @author       ajz
 // @require      https://code.jquery.com/jquery-3.7.1.slim.min.js
@@ -10,13 +10,12 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
+  var $ = jQuery;
 
-    var $ = jQuery;
-
-    $(document).ready(function() {
-        $('h3:contains("Sponsored")').parents('article').remove();
-        $('a:contains("OneTaste")').parents('article').remove();
-    });
-
+  $(document).ready(function () {
+    $('h3:contains("Sponsored")').parents("article").remove();
+    $('a:contains("OneTaste")').parents("article").remove();
+    $('a[href*="axios"]').parents("article").remove();
+  });
 })();
