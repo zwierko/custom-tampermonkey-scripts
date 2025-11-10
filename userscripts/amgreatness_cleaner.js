4ccbe3b0fa4c715adef11830c2f8c2141f885236
Adam <59660442+zwierko@users.noreply.github.com>
2025-11-10 15:17:28 -0500

    AmGreatness cleaner URL typo correction.

--- userscripts/amgreatness_cleaner.js
+++ userscripts/amgreatness_cleaner.js
@@ -5,7 +5,7 @@
 // @description  Clean up articles I don't want to see (orig. 31-May-2025)
 // @author       ajz
 // @require      https://code.jquery.com/jquery-3.7.1.slim.min.js
-// @match        https://amgreatness.com/
+// @match        https://amgreatness.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
